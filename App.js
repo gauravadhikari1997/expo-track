1728d00c3fb44c9e0fa59cffc4b0669ab92faae8
unknown <gaurav.15bme2023@abes.ac.in>
2020-07-05 09:53:31 +0530

    added styling to Screens and created context

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
@@ -9,6 +9,8 @@ import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
+
+import TrackContext from "./src/context/TrackContext";
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -25,4 +27,22 @@ const switchNavigator = createSwitchNavigator({
   }),
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  const initialState = [];
+
+  function ourReducer(state, action) {
+    switch (action.type) {
+      default:
+        return state;
+    }
+  }
+  const [state, dispatch] = useReducer(ourReducer, initialState);
+
+  return (
+    <TrackContext.Provider>
+      <App value={state} />
+    </TrackContext.Provider>
+  );
+};
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,57 @@
-import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View>
-      <Text>SignupScreen</Text>
-      <Button
-        title="Go to SigninScreen"
-        onPress={() => navigation.navigate("Signin")}
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3 style={styles.title}>
+          Sign Up for Track
+        </Text>
+      </Spacer>
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={email}
+        onChangeText={(newText) => setEmail(newText)}
+        label="Email"
       />
-      <Button
-        title="Go to mainFlow"
-        onPress={() => navigation.navigate("mainFlow")}
+      <Spacer />
+      <Input
+        secureTextEntry
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={(newText) => setPassword(newText)}
+        label="Password"
       />
+      <Spacer>
+        <Button title="Signup" />
+      </Spacer>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return { header: () => false };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 80,
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 20,
+  },
+});
 
 export default SignupScreen;
